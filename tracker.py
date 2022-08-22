6ef610fb092ea69641e46d716dd239fe05a96daa
Filip Dobrocký <filip.dobrocky@gmail.com>
2022-08-22 14:55:32 +0200

    tracker class

--- tracker.py
+++ tracker.py
@@ -14,90 +14,97 @@ from yolov5.utils.dataloaders import LoadStreams, LoadImages
 from yolov5.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from yolov5.utils.plots import Annotator, colors, save_one_box
-from yolov5.utils.torch_utils import select_device#, load_classifier, time_synchronized
+from yolov5.utils.torch_utils import select_device, time_sync#, load_classifier
 
 import numpy as np
 from motpy import Detection, MultiObjectTracker
 
+
 class Tracker:
-    pass
+    def __init__(self, source = '0',
+                       weights = 'yolov5n.pt',
+                       imgsz = 320,
+                       device = 'cpu',
+                       augment = True,
+                       conf_thres = 0.6,
+                       iou_thres = 0.6,
+                       agnostic_nms = True):
+        self.is_video = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+            ('rtsp://', 'rtmp://', 'http://', 'https://'))
 
-def detect(save_img=False):
-    source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://'))
+        self.weights, self.augment, self.conf_thres, self.iou_thres, self.agnostic_nms \
+            = weights, augment, conf_thres, iou_thres, agnostic_nms
 
-    # Directories
-    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+        # Initialize
+        set_logging()
+        self.__device = select_device(device)
+        self.__half = self.__device.type != 'cpu'  # half precision only supported on CUDA
 
-    # Initialize
-    set_logging()
-    device = select_device(opt.device)
-    half = device.type != 'cpu'  # half precision only supported on CUDA
+        # Create a multi object tracker
+        self.__tracker = MultiObjectTracker(dt=0.1)
 
-    #create a multi object tracker
-    tracker = MultiObjectTracker(dt=0.1)
+        # Load model
+        self.model = attempt_load(weights, device='cpu')  # load FP32 model
+        stride = int(self.model.stride.max())  # model stride
+        self.imgsz = check_img_size(imgsz, s=stride)  # check img_size
+        if self.__half:
+            self.model.half()  # to FP16
 
-    # Load model
-    model = attempt_load(weights, device='cpu')  # load FP32 model
-    stride = int(model.stride.max())  # model stride
-    imgsz = check_img_size(imgsz, s=stride)  # check img_size
-    if half:
-        model.half()  # to FP16
+        # Second-stage classifier
+        # self.classify = False
+        # if classify:
+        #     modelc = load_classifier(name='resnet101', n=2)  # initialize
+        #     modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
 
-    # Second-stage classifier
-    #classify = False
-    #if classify:
-        #modelc = load_classifier(name='resnet101', n=2)  # initialize
-        #modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+        # Set Dataloader
+        if self.is_video:
+            self.view_img = check_imshow()
+            cudnn.benchmark = True  # set True to speed up constant image size inference
+            self.dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+        else:
+            self.save_img = True
+            self.dataset = LoadImages(source, img_size=imgsz, stride=stride)
 
-    # Set Dataloader
-    vid_path, vid_writer = None, None
-    if webcam:
-        view_img = check_imshow()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-    else:
-        save_img = True
-        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+        # Get names and colors
+        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
 
-    # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
+        if self.__device.type != 'cpu':
+            self.model(torch.zeros(1, 3, self.imgsz, self.imgsz)
+                       .to(self.__device).type_as(next(self.model.parameters())))  # run once
 
-    # Run inference
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-    t0 = time.time()
-    for path, img, im0s, vid_cap, *aux in dataset:
-        img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
+        self.__tracks = None
+
+    def get_class_index(self, name):
+        return list(self.names.keys())[list(self.names.values()).index(name)]
+
+    def track(self, classes = None):
+        # Run inference
+        path, img, im0s, vid_cap, *aux = next(self.dataset)
+        img = torch.from_numpy(img).to(self.__device)
+        img = img.half() if self.__half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
 
         # Inference
-        #t1 = time_synchronized()
-        pred = model(img, augment=opt.augment)[0]
-
+        t1 = time_sync()
+        pred = self.model(img, augment=self.augment)[0]
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
-        #t2 = time_synchronized()
+        pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=classes,
+                                   agnostic=self.agnostic_nms)
+        t2 = time_sync()
 
         # Apply Classifier
-        #if classify:
+        # if self.classify:
         #    pred = apply_classifier(pred, modelc, img, im0s)
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+            if self.is_video:  # batch_size >= 1
+                s, im0, frame = '%g: ' % i, im0s[i].copy(), self.dataset.count
             else:
-                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+                s, im0, frame = '', im0s, getattr(self.dataset, 'frame', 0)
 
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # img.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
@@ -107,7 +114,7 @@ def detect(save_img=False):
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                    s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 # Write results
                 out_detections = []
@@ -115,53 +122,41 @@ def detect(save_img=False):
                     object_box = np.array([int(xyxy[0]), int(xyxy[1]), int(xyxy[2]), int(xyxy[3])])
                     out_detections.append(Detection(box=object_box, score=conf.to('cpu'), class_id=int(cls)))
 
-                tracker.step(out_detections)
-                tracks = tracker.active_tracks(3)
+                self.__tracker.step(out_detections)
+                self.__tracks = self.__tracker.active_tracks(3)
 
-                for track in tracks:
-                    label = f'{track.id[:5]}: {names[track.class_id]}'
+                for track in self.__tracks:
+                    label = f'{track.id[:5]}: {self.names[track.class_id]}'
                     annotator = Annotator(im0, line_width=2, pil=not ascii)
                     annotator.box_label(track.box, label, color=colors(track.class_id, True))
                     im0 = annotator.result()
 
-
-            # Print time (inference + NMS)
-            #print(f'{s}Done. ({t2 - t1:.3f}s)')
+                # Print time (inference + NMS)
+                print(f'{s}Done. ({t2 - t1:.3f}s)')
 
             # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
+            if self.view_img:
+                cv2.imshow('result', im0)
                 cv2.waitKey(1)  # 1 millisecond
 
-    print(f'Done. ({time.time() - t0:.3f}s)')
-
+        return self.__tracks
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='0', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    opt = parser.parse_args()
-    print(opt)
     check_requirements()
+    #with torch.no_grad():
 
-    with torch.no_grad():
-        if opt.update:  # update all models (to fix SourceChangeWarning)
-            for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
-                detect()
-                strip_optimizer(opt.weights)
-        else:
-            detect()
+    #    strip_optimizer(tracker.weights)
+
+    t0 = time.time()
+    #tracker = Tracker(source='https://www.youtube.com/watch?v=K4NiaXmXIhE')
+    #class_index = tracker.get_class_index('car')
+    #print("class: " + str(class_index))
+    tracker = Tracker(source='0')
+    while True:
+        try:
+            tracker.track()
+        except StopIteration:
+            print(f'Done. ({time.time() - t0:.3f}s)')
+            break
+
+
