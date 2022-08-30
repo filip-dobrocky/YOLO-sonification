56244494431ad189bc2749157e3e02d8f6ea930d
Filip Dobrocky <filip.dobrocky@gmail.com>
2022-08-30 10:30:26 +0200

    set default freq to zero

--- synths.py
+++ synths.py
@@ -5,7 +5,7 @@ from supriya.synthdefs import synthdef
 import time
 
 @synthdef()
-def beeper(freq = 440, tempo = 120, level = 0.1, pan = 0):
+def beeper(freq = 0, tempo = 120, level = 0.1, pan = 0):
     gen = SinOsc.ar(freq) * level * 0.5 * (Saw.kr(tempo / 60) + 0.5)
     Out.ar(0, Pan2.ar(gen, pan))
 
