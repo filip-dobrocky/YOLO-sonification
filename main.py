d042031b7b76b91f39e09ec64234e018fd58f8b5
Filip Dobrocky <filip.dobrocky@gmail.com>
2022-09-01 18:03:25 +0200

    added new synth and fx bus with reverb

--- main.py
+++ main.py
@@ -7,27 +7,42 @@ import torch
 if __name__ == '__main__':
     print(torch.cuda.is_available())
     server = supriya.Server().boot()
+
+    server.add_synthdef(synths.reverb)
+    server.add_synthdef(synths.popcorn)
     server.add_synthdef(synths.beeper)
 
+    fx_bus = server.add_bus_group(2, 'audio')
+    rev = server.add_synth(synths.reverb, in_bus=fx_bus.bus_id)
+
     tracker = Tracker(source='https://www.youtube.com/watch?v=cONRX_No2-w')
-    # tracker = Tracker(source='0')
+    #tracker = Tracker(source="0")
 
     synth_map = dict()
 
     while True:
         try:
-            tracker.track()
-            for o in tracker.new_objs:
-                synth_map[o.id] = server.add_synth(synths.beeper)
+            all, new, deleted = tracker.track()
+            for o in new:
+                if (o.class_id == 0):
+                    synth_map[o.id] = server.add_synth(synths.beeper, add_action='addBefore', fx_bus=fx_bus.bus_id)
+                else:
+                    synth_map[o.id] = server.add_synth(synths.popcorn, add_action='addBefore', fx_bus=fx_bus.bus_id)
                 # print("added synth")
-            for o in tracker.del_objs:
+            for o in deleted:
                 synth_map[o.id].release()
                 # print("released synth")
-            for id, o in tracker.all_objs.items():
+            for id, o in all.items():
                 if id in synth_map:
-                    synth_map[id]['freq'] = 50 + 1000 * (1 - o.pos[1] / tracker.video_size[1])
-                    synth_map[id]['pan'] = (o.pos[0] / tracker.video_size[0]) * 2 - 1
-                    synth_map[id]['tempo'] = 30 + o.speed * 100
+                    synth = synth_map[id]
+                    if synth.synthdef.name == 'beeper':
+                        synth['freq'] = 20 + 2000 * (1 - o.pos[1] / tracker.video_size[1])
+                        synth['pan'] = (o.pos[0] / tracker.video_size[0]) * 2 - 1
+                        synth['tempo'] = 30 + o.speed * 50
+                    elif synth.synthdef.name == 'popcorn':
+                        synth['freq'] = 50 + 5000 * (1 - o.pos[1] / tracker.video_size[1])
+                        synth['pan'] = (o.pos[0] / tracker.video_size[0]) * 2 - 1
+                        synth['tempo'] = 100 + o.speed * 50
                     # print(f'set params {synth_map[id]["freq"]} {synth_map[id]["pan"]} {synth_map[id]["tempo"]}')
 
         except StopIteration:
--- synths.py
+++ synths.py
@@ -1,18 +1,47 @@
 import supriya
-from supriya.ugens import Out, SinOsc, Saw
+from supriya.ugens import In, Out, SinOsc, Saw, Dust
+from supriya.ugens.noise import WhiteNoise, Rand
+from supriya.ugens.filters import BPF, Decay2
 from supriya.ugens.panning import Pan2
+from supriya.ugens.delay import AllpassC
 from supriya.synthdefs import synthdef
 import time
 
 @synthdef()
-def beeper(freq = 0, tempo = 120, level = 0.1, pan = 0):
+def beeper(fx_bus, out_bus=0, direct=0.7, freq=1, tempo=120, level=0.1, pan=0):
     gen = SinOsc.ar(freq) * level * 0.5 * (Saw.kr(tempo / 60) + 0.5)
-    Out.ar(0, Pan2.ar(gen, pan))
+    Out.ar(out_bus, Pan2.ar(gen * direct, pan))
+    Out.ar(fx_bus, Pan2.ar(gen * (1 - direct), pan))
+
+@synthdef()
+def popcorn(fx_bus, out_bus=0, direct=0.5, freq=50, tempo=120, level=0.5, pan=0):
+    source = BPF.ar(level * WhiteNoise.ar(), freq, 1.0)
+    gen = Decay2.ar(Dust.ar(tempo / 60), 0.01, 0.2) * source
+    Out.ar(out_bus, Pan2.ar(gen * direct, pan))
+    Out.ar(fx_bus, Pan2.ar(gen * (1 - direct), pan))
+
+@synthdef()
+def reverb(in_bus, out_bus=0):
+    buf = In.ar(in_bus, 2)
+    for i in range(16):
+        buf = AllpassC.ar(buf, 0.04, [Rand(0.001, 0.04), Rand(0.001, 0.04)], 3)
+    Out.ar(out_bus, buf)
+
 
 if __name__ == '__main__':
     server = supriya.Server().boot()
+    server.add_synthdef(reverb)
+    server.add_synthdef(popcorn)
     server.add_synthdef(beeper)
-    synth = server.add_synth(beeper)
+
+    fx_bus = server.add_bus_group(2, 'audio')
+    rev = server.add_synth(reverb, in_bus=fx_bus.bus_id)
+    synth1 = server.add_synth(popcorn, add_action='addBefore', fx_bus=fx_bus.bus_id)
+    synth2 = server.add_synth(beeper, add_action='addBefore', fx_bus=fx_bus.bus_id)
+    time.sleep(1)
+    synth1['freq'] = 800
+    synth2['freq'] = 200
     time.sleep(10)
-    synth.release()
+    synth1.release()
+    synth2.release()
     server.quit()
